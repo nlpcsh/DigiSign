368af0568e2ae45dd4ffab5ef5de4e452e28d522
Nikolay Petrov <nikolay.petrov@abv.bg>
2026-05-07 16:19:37 +0300

    fix icon problem

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from classes.PdfSigner import PdfSigner
 def main() -> None:
     root = tk.Tk()
     PdfSigner(root)
+    root.iconbitmap("icon.ico")
     root.mainloop()
 
 if __name__ == "__main__":
